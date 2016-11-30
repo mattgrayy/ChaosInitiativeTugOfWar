7fbccbe8d6d470e990400fecae2419c77705014a
Tredwood <Thomas2.Redwood@live.uwe.ac.uk>
2016-11-30 11:21:03 +0000

    player 1 and 2 movement

---
+++ TugOfWar/Assets/Scripts/Player1.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+      transform.Translate(Vector3.right * Input.GetAxis("Horizontal_0")/10);
+
+        if((Input.GetAxis("Vertical_0")>0)&&(Input.GetButtonDown("NES B_0")))
+        {
+            //Throw bomb
+            transform.Translate(Vector3.right / 10);
+        }
+        else if(Input.GetButtonDown("NES B_0"))
+        {
+            //place bomb
+            transform.Translate(Vector3.up / 10);
+        }
+        if (Input.GetButtonDown("NES A_0"))
+        {
+            //nudge
+            transform.Translate(Vector3.down / 10);
+        }
+    }
+}
---
+++ TugOfWar/Assets/Scripts/Player2.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.right * Input.GetAxis("Horizontal_1") / 10);
+
+        if ((Input.GetAxis("Vertical_1") > 0) && (Input.GetButtonDown("NES B_1")))
+        {
+            //Throw bomb
+            transform.Translate(Vector3.right / 10);
+        }
+        else if (Input.GetButtonDown("NES B_1"))
+        {
+            //place bomb
+            transform.Translate(Vector3.up / 10);
+        }
+        if (Input.GetButtonDown("NES A_1"))
+        {
+            //nudge
+            transform.Translate(Vector3.down / 10);
+        }
+    }
+}
