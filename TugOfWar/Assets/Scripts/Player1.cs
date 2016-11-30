55d46967cf14645fe32b63d6df5aea21b491776f
Fentone <Elliot2.Fenton@live.uwe.ac.uk>
2016-11-30 13:11:24 +0000

    Added bomb throwing and player death

--- TugOfWar/Assets/Scripts/Player1.cs
+++ TugOfWar/Assets/Scripts/Player1.cs
@@ -2,9 +2,14 @@
 using System.Collections;
 
 public class Player1 : MonoBehaviour {
+	[SerializeField] Transform bomb;
+	bool hasBomb;
+	float deathTimer;
+	void PlayerDeath();
 
 	// Use this for initialization
 	void Start () {
+		hasBomb = false;
 	
 	}
 	
@@ -12,15 +17,19 @@ public class Player1 : MonoBehaviour {
 	void Update () {
       transform.Translate(Vector3.right * Input.GetAxis("Horizontal_0")/10);
 
-        if((Input.GetAxis("Vertical_0")>0)&&(Input.GetButtonDown("NES B_0")))
+		if((Input.GetAxis("Vertical_0")>0)&&(Input.GetButtonDown("NES B_0"))&&(hasBomb==true))
         {
             //Throw bomb
-            transform.Translate(Vector3.right / 10);
+			Transform clone = Instantiate(bomb, transform.position, transform.rotation) as Transform;
+			Vector2 Direction = clone.transform.right + clone.transform.up;
+			clone.GetComponent<Rigidbody2D> ().AddForce ((Direction) * 400);
+			hasBomb = false;
         }
-        else if(Input.GetButtonDown("NES B_0"))
+		else if((Input.GetButtonDown("NES B_0"))&&(hasBomb == true))
         {
             //place bomb
-            transform.Translate(Vector3.up / 10);
+			Instantiate(bomb, transform.position, bomb.rotation);
+			hasBomb = false;
         }
         if (Input.GetButtonDown("NES A_0"))
         {
@@ -28,4 +37,25 @@ public class Player1 : MonoBehaviour {
             transform.Translate(Vector3.down / 10);
         }
     }
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.gameObject.tag == "pickup") {
+			hasBomb = true;
+		}
+	}
+
+	void PlayerDeath()
+	{
+		Renderer.enabled = false;
+		BoxCollider2D.enabled = false;
+		hasBomb = false;
+		deathTimer = 0f;
+		deathTimer += Time.deltaTime;
+		if (deathTimer >= 4f) {
+			transform.position = Vector2 (-6.5, -3.5);
+			Renderer.enabled = true;
+			BoxCollider2D.enabled = true;
+		}
+	}
 }
