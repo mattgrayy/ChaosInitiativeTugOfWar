c826692f13b21ee95998ca0eff864f5daabac573
OwenRuddle <Owen2.Ruddle@live.uwe.ac.uk>
2016-11-30 13:06:36 +0000

    mainscene
    added main scene

---
+++ TugOfWar/Assets/Scripts/HighScore.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HighScore : MonoBehaviour {
+   // [SerializeField]   private
+        public Text HighScoreText;
+ //   [SerializeField] private
+      private  int HighScorein ;
+    score SCORE;
+    GameObject Player = GameObject.Find("Player2");
+    // Use this for initialization
+
+
+    void Start () {
+        DontDestroyOnLoad(gameObject);
+        HighScorein = SCORE.Score;
+      
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        HighScoreText.text = HighScorein.ToString();
+    }
+}
---
+++ TugOfWar/Assets/Scripts/sceene manager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sceenemanager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	//if(/*player press start*/)
+ //       {
+ //           //spawn game bits
+ //       }
+ //   else if(/*player wins*/)
+ //       {
+ //           //spawn win screen 
+ //       }
+
+	}
+}
